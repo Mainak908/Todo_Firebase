0bc81cd6d1ae86b34fe32c5fdf035367d8faf3cd
Mainak Ghosh <ghosh.mainak0401@gmail.com>
2023-02-01 10:59:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { db } from './firebase-config';
+import firebase from "firebase/compat/app"
+import { useEffect } from 'react';
+import React from 'react';
 
 function App() {
+
+  const [data, setdata] = useState("");
+  const [allDocs, setAllDocs] = useState([]);
+  useEffect(() => {
+     getTodo();
+  }, [])
+
+  const randomfunc = (event) => {
+    setdata(event.target.value);
+  }
+
+  const addTodo = () => {
+    db.collection("Todo").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      todo: data,
+    })
+    
+  }
+  // const getTodo = () => {
+  //   db.collection("Todo")
+  //     .get()
+  //     .then((snapshot) => {
+  //       if (snapshot.docs.length > 0) {
+  //         snapshot.docs.forEach((doc) => {
+  //           setAllDocs((prev) => {
+  //             return [...prev, doc.data()]
+  //           })
+  //         })
+  //       }
+  //     })
+  // }
+  const getTodo = () => {
+    db.collection("Todo")
+    .onSnapshot(function(querySnapshot){
+      setAllDocs(
+        querySnapshot.docs.map((doc)=>({
+          id:doc.id,
+          todo:doc.data().todo
+        }))
+      )
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>TODO APP</h3>
+      <div className="nothing">
+        <input className="form-control" type="text" value={data} onChange={randomfunc} label="readonly input example"></input>
+        <input className="btn btn-primary" type="submit" onClick={addTodo} value="save"></input>
+      </div>
+      <h1>FETCHING DATA</h1>
+
+      {allDocs.map((doc,i) => {
+        return (
+          <div key={i}>
+            <h5>{doc.todo}</h5>
+          </div>
+        )
+      })}
+
+      
+
+
+    </>
   );
 }
 
